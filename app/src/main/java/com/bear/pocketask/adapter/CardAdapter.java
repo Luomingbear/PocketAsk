c596a7984d2e5c015cbceaabd34baeb34eac5856
罗明 <luomingbear@163.com>
2016-10-08 19:30:51 +0800

    完成卡片的拖动页和适配器

--- app/src/main/java/com/bear/pocketask/adapter/CardAdapter.java
+++ app/src/main/java/com/bear/pocketask/adapter/CardAdapter.java
@@ -21,141 +21,124 @@ import android.widget.TextView;
  * Created by bear on 16/10/7.
  */
 
-public class CardAdapter extends BaseAdapter implements View.OnClickListener
-{
-	private static final String TAG = "CardAdapter";
-	private List<CardItemInfo> list; //数据源列表
-	CardItemInfo info;
-	private LayoutInflater inflater;
+public class CardAdapter extends BaseAdapter implements View.OnClickListener {
+    private static final String TAG = "CardAdapter";
+    private List<CardItemInfo> list; //数据源列表
+    CardItemInfo info;
+    private LayoutInflater inflater;
 
-	public CardAdapter(Context context, List<CardItemInfo> list)
-	{
-		inflater = LayoutInflater.from(context);
-		this.list = list;
-	}
+    public CardAdapter(Context context, List<CardItemInfo> list) {
+        inflater = LayoutInflater.from(context);
+        this.list = list;
+    }
 
-	@Override
-	public int getCount()
-	{
-		return list == null ? 0 : list.size();
-	}
+    @Override
+    public int getCount() {
+        return list == null ? 0 : list.size();
+    }
 
-	@Override
-	public Object getItem(int position)
-	{
-		return list == null ? null : list.get(position);
-	}
+    @Override
+    public Object getItem(int position) {
+        return list == null ? null : list.get(position);
+    }
 
-	@Override
-	public long getItemId(int position)
-	{
-		return list == null ? 0 : position;
-	}
+    @Override
+    public long getItemId(int position) {
+        return list == null ? 0 : position;
+    }
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent)
-	{
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
 
-		if (convertView == null)
-		{
-			convertView = inflater.inflate(R.layout.card_item, null);
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.card_item, null);
 
-			viewHolder = new ViewHolder();
-			viewHolder.ivDetailPic = (ImageView) convertView.findViewById(R.id.card_item_detail_pic);
-			viewHolder.ivHeadPic = (CircleImageView) convertView.findViewById(R.id.card_item_head_pic);
-			viewHolder.tvQuestion = (TextView) convertView.findViewById(R.id.card_item_questions);
-			viewHolder.tvUserName = (TextView) convertView.findViewById(R.id.card_item_user_name);
+            viewHolder = new ViewHolder();
+            viewHolder.ivDetailPic = (ImageView) convertView.findViewById(R.id.card_item_detail_pic);
+            viewHolder.ivHeadPic = (CircleImageView) convertView.findViewById(R.id.card_item_head_pic);
+            viewHolder.tvQuestion = (TextView) convertView.findViewById(R.id.card_item_questions);
+            viewHolder.tvUserName = (TextView) convertView.findViewById(R.id.card_item_user_name);
 
-			viewHolder.report = convertView.findViewById(R.id.card_item_report);
-			viewHolder.btSend = convertView.findViewById(R.id.card_item_send);
-			convertView.setTag(viewHolder);
-		} else
-			viewHolder = (ViewHolder) convertView.getTag();
+            viewHolder.report = convertView.findViewById(R.id.card_item_report);
+            viewHolder.btSend = convertView.findViewById(R.id.card_item_send);
+            convertView.setTag(viewHolder);
+        } else
+            viewHolder = (ViewHolder) convertView.getTag();
 
-		//设置值
-		info = list.get(position);
-		viewHolder.tvUserName.setText(info.getUserName());
-		viewHolder.tvQuestion.setText(info.getQuestions());
-		ImageLoader.getInstance().displayImage(info.getHeadPic(), viewHolder.ivHeadPic);
-		ImageLoader.getInstance().displayImage(info.getDetailPic(), viewHolder.ivDetailPic);
+        //设置值
+        info = list.get(position);
+        viewHolder.tvUserName.setText(info.getUserName());
+        viewHolder.tvQuestion.setText(info.getQuestions());
+        ImageLoader.getInstance().displayImage(info.getHeadPic(), viewHolder.ivHeadPic);
+        ImageLoader.getInstance().displayImage(info.getDetailPic(), viewHolder.ivDetailPic);
 
-		//添加点击事件
-		viewHolder.report.setOnClickListener(this);
-		viewHolder.ivDetailPic.setOnClickListener(this);
-		viewHolder.ivHeadPic.setOnClickListener(this);
-		viewHolder.btSend.setOnClickListener(this);
-		return convertView;
-	}
+        //添加点击事件
+        viewHolder.report.setOnClickListener(this);
+        viewHolder.ivDetailPic.setOnClickListener(this);
+        viewHolder.ivHeadPic.setOnClickListener(this);
+        viewHolder.btSend.setOnClickListener(this);
+        return convertView;
+    }
 
-	private ViewHolder viewHolder;
+    private ViewHolder viewHolder;
 
-	@Override
-	public void onClick(View v)
-	{
-		switch (v.getId())
-		{
-		case R.id.card_item_report:
-		{
-			if (cardItemClickListener != null)
-				cardItemClickListener.onClickedObject(CardItemClickMode.REPORT_BUTTON, info.getQuestionId());
-			break;
-		}
-		case R.id.card_item_detail_pic:
-		{
-			if (cardItemClickListener != null)
-				cardItemClickListener.onClickedObject(CardItemClickMode.DETAIL_PIC, info.getQuestionId());
-			Log.i(TAG, "onClick: detail");
-			break;
-		}
-		case R.id.card_item_head_pic:
-		{
-			if (cardItemClickListener != null)
-				cardItemClickListener.onClickedObject(CardItemClickMode.HEAD_PIC, info.getQuestionId());
-			break;
-		}
-		case R.id.card_item_send:
-		{
-			if (cardItemClickListener != null)
-				cardItemClickListener.onClickedObject(CardItemClickMode.SEND_BUTTON, info.getQuestionId());
-			break;
-		}
-		}
-	}
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.card_item_report: {
+                if (cardItemClickListener != null)
+                    cardItemClickListener.onClickedObject(CardItemClickMode.REPORT_BUTTON, info.getQuestionId());
+                break;
+            }
+            case R.id.card_item_detail_pic: {
+                if (cardItemClickListener != null)
+                    cardItemClickListener.onClickedObject(CardItemClickMode.DETAIL_PIC, info.getQuestionId());
+                Log.i(TAG, "onClick: detail");
+                break;
+            }
+            case R.id.card_item_head_pic: {
+                if (cardItemClickListener != null)
+                    cardItemClickListener.onClickedObject(CardItemClickMode.HEAD_PIC, info.getQuestionId());
+                break;
+            }
+            case R.id.card_item_send: {
+                if (cardItemClickListener != null)
+                    cardItemClickListener.onClickedObject(CardItemClickMode.SEND_BUTTON, info.getQuestionId());
+                break;
+            }
+        }
+    }
 
-	private class ViewHolder
-	{
-		CircleImageView ivHeadPic;
-		TextView tvQuestion;
-		TextView tvUserName;
-		ImageView ivDetailPic;
-		View btSend;
-		View report;
-	}
+    private class ViewHolder {
+        CircleImageView ivHeadPic;
+        TextView tvQuestion;
+        TextView tvUserName;
+        ImageView ivDetailPic;
+        View btSend;
+        View report;
+    }
 
-	private CardItemClickListener cardItemClickListener;
+    private CardItemClickListener cardItemClickListener;
 
-	public interface CardItemClickListener
-	{
-		void onClickedObject(CardItemClickMode clickMode, int questionId);
-	}
+    public interface CardItemClickListener {
+        void onClickedObject(CardItemClickMode clickMode, int questionId);
+    }
 
-	public void setCardItemClickListener(CardItemClickListener cardItemClickListener)
-	{
-		this.cardItemClickListener = cardItemClickListener;
-	}
+    public void setCardItemClickListener(CardItemClickListener cardItemClickListener) {
+        this.cardItemClickListener = cardItemClickListener;
+    }
 
-	public enum CardItemClickMode
-	{
-		//头像
-		HEAD_PIC,
+    public enum CardItemClickMode {
+        //头像
+        HEAD_PIC,
 
-		//问题图片详情
-		DETAIL_PIC,
+        //问题图片详情
+        DETAIL_PIC,
 
-		///举报按钮
-		REPORT_BUTTON,
+        ///举报按钮
+        REPORT_BUTTON,
 
-		//发送按钮
-		SEND_BUTTON
-	}
+        //发送按钮
+        SEND_BUTTON
+    }
 }
